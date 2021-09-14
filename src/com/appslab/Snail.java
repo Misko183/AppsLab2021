7bcb86e3e591cdccf083f184e698c5080a4660fc
Misko183 <rtmike183@gmail.com>
2021-09-14 09:53:02 +0200

    Added new class named Snail with calculating distance of height tower.

---
+++ src/com/appslab/Snail.java
@@ -0,0 +1,13 @@
+package com.appslab;
+
+public class Snail {
+
+    public static void main(String[] args) {
+
+        double height = 0.4;
+        double lenght = 0.2;
+        double towerHeight = 100;
+        double distance = (towerHeight/height)+(towerHeight/height)*lenght;
+        System.out.println(distance);
+    }
+}
